13bb38a01ed8f230670a8e3ebe8a27c392edc5a4
Andry <andry.rama.rahary@gmail.com>
2021-07-20 09:39:50 +0200

    connexion ok

--- connexion.php
+++ connexion.php
@@ -3,9 +3,58 @@
     <br><br><br><br>
     <h1>Connexion</h1>
 
+<?php
+    //--------------------------------- TRAITEMENTS PHP ---------------------------------//
+if($_POST)
+{
+    // $contenu .=  "pseudo : " . $_POST['pseudo'] . "<br>mdp : " .  $_POST['mdp'] . "";
+    $resultat = executeRequete("SELECT * FROM membre WHERE pseudo='$_POST[pseudo]'");
+    if($resultat->rowCount() != 0)
+    {
+        // $contenu .=  '<div style="background:green">pseudo connu!</div>';
+        $membre = $resultat->fetch(PDO::FETCH_ASSOC);
+        if($membre['mdp'] == $_POST['mdp'])
+        {
+            //$contenu .= '<div class="validation">mdp connu!</div>';
+            foreach($membre as $indice => $element)
+            {
+                if($indice != 'mdp')
+                {
+                    $_SESSION['membre'][$indice] = $element;
+                }
+            }
+            header("location:profil.php");
+        }
+        else
+        {
+            $contenu .= '<div class="erreur">Erreur de MDP</div>';
+        }       
+    }
+    else
+    {
+        $contenu .= '<div class="erreur">Erreur de pseudo</div>';
+    }
+}
+//--------------------------------- AFFICHAGE HTML ---------------------------------//
+?>
+<?php require_once("inc/haut.inc.php"); ?>
+<?php echo $contenu; ?>
+
+<form method="post" action="">
+    <label for="pseudo">Pseudo</label><br>
+    <input type="text" id="pseudo" name="pseudo"><br> <br>
+
+    <label for="mdp">Mot de passe</label><br>
+    <input type="text" id="mdp" name="mdp"><br><br>
+
+    <input type="submit" value="Se connecter">
+</form>
+
     <h3>
         <a href="<?php echo RACINE_SITE; ?>inscription.php">
         Inscription
         </a>
     </h3>
+
+
     <?php require_once("inc/bas.inc.php"); ?>
