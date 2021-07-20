62aaeb62edf984dd81fe070402c584ad05000671
Andry <andry.rama.rahary@gmail.com>
2021-07-20 10:07:41 +0200

    page profil affiche info

--- profil.php
+++ profil.php
@@ -1,5 +1,19 @@
-<?php require_once("inc/init.inc.php"); ?>
+<?php
+require_once("inc/init.inc.php");
+//--------------------------------- TRAITEMENTS PHP ---------------------------------//
+if(!internauteEstConnecte()) header("location:connexion.php");
+// debug($_SESSION);
+$contenu .= '<p class="centre">Bonjour <strong>' . $_SESSION['membre']['pseudo'] . '</strong></p>';
+$contenu .= '<div class="cadre"><h2> Voici vos informations </h2>';
+$contenu .= '<p> votre email est: ' . $_SESSION['membre']['email'] . '<br>';
+$contenu .= 'votre ville est: ' . $_SESSION['membre']['ville'] . '<br>';
+$contenu .= 'votre code postal est: ' . $_SESSION['membre']['code_postal'] . '<br>';
+$contenu .= 'votre adresse est: ' . $_SESSION['membre']['adresse'] . '</p></div><br><br>';
+?>
+<!-- //--------------------------------- AFFICHAGE HTML ---------------------------------// -->
 <?php require_once("inc/haut.inc.php"); ?>
-    <br><br><br><br>
-    <h1>Profil</h1>
-    <?php require_once("inc/bas.inc.php"); ?>
+<br><br><br><br><br>
+<h1>Page profil</h1>
+
+<?php echo $contenu; ?>
+<?php require_once("inc/bas.inc.php"); ?>
