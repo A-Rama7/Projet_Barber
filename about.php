28ebf2a196d074ab0c787c5eee3ea97ff9f03056
Andry <andry.rama.rahary@gmail.com>
2021-07-21 15:13:52 +0200

    ok

--- about.php
+++ about.php
@@ -1,82 +1,73 @@
 <?php require_once("inc/init.inc.php"); ?>
 <?php require_once("inc/haut.inc.php"); ?>
-    <section class="top-about">
-        <div class="title_about">
-            <div class="text-title_about">
-                <h1 class="titles">About</h1>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-                    Iste sint iure quidem porro in totam 
-                    commodi dolorem recusandae impedit nulla.
-                </p>
-            </div>
+<section class="top-about">
+    <div class="title_about">
+        <div class="text-title_about">
+            <h1 class="titles">About</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Iste sint iure quidem porro in totam 
+                commodi dolorem recusandae impedit nulla.
+            </p>
         </div>
-        <div class="image_top-about">
-        </div>
-    </section>
+    </div>
+    <div class="image_top-about">
+    </div>
+</section>
     
-    <section class="hair-space">
-    <div id="passions">
-            <div class="header-passions">
-                <p class="heading-num">02</p>
-                <div class="title_passions">
-                    <h2 class="title_section">Passions</h2>
+<section class="hair-space">
+        <div class="header_hair-space">
+            <div class="title_hair-space">
+                <h2 class="titles">Hair Space</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Atque quaerat cumque commodi, repellat cupiditate odio nihil 
+                    aliquid facere quam alias?</p>
+            </div>
+        </div>
+        <div class="hair-space_content">
+            <div class="card_hair-space">
+                <div class="image_hair-space">
+                    <img src="./inc/images/relax.svg" alt="design">
+                </div>
+                <div class="sub_title_hair-space">
+                    <h3>relax</h3>
+                </div>
+                <div class="text_hair-space">
+                    <p>Lorem ipsum dolor sit amet, 
+                        consectetur adipisicing elit. 
+                        Adipisci, qui.
+                        </p>
                 </div>
             </div>
-            <div class="passions_content">
-                <div class="card_passions">
-                    <div class="image_passions">
-                        <img src="./inc/images/relax.svg" alt="design">
-                    </div>
-                    <div class="sub_title_passions">
-                        <h3>relax</h3>
-                    </div>
-                    <div class="text_passions">
-                        <p>Je considère le design comme une "baguette magique". 
-                            Un produit peut-être efficace pour répondre à une problématique 
-                            mais sans un bons design, il n'aura pas ce petit quelque chose 
-                            qui le rendra unique et exceptionnel au yeux du client.
-                            Ainsi, je m'efforce chaque jour à accorder une importance primoridal au design 
-                            lorsque je créer un produit comme un site web.
-                            </p>
+            <div class="card_hair-space">
+                <div class="image_hair-space">
+                    <div class="image_hair-space">
+                        <img src="./inc/images/refresh.svg" alt="tech">
                     </div>
                 </div>
-                <div class="card_passions">
-                    <div class="image_passions">
-                        <div class="image_passions">
-                            <img src="./inc/images/refresh.svg" alt="tech">
-                        </div>
+                <div class="sub_title_hair-space">
+                    <h3>refresh</h3>
+                </div>
+                <div class="text_hair-space">
+                    <p>Lorem ipsum dolor sit amet c
+                        onsectetur adipisicing elit. 
+                        Deserunt, optio.
+                    </p>
+                </div>
+            </div>
+            <div class="card_hair-space">
+                <div class="image_hair-space">
+                    <div class="image_hair-space">
+                        <img src="./inc/images/smile.svg" alt="book">
                     </div>
-                    <div class="sub_title_passions">
-                        <h3>refresh</h3>
-                    </div>
-                    <div class="text_passions">
-                        <p>Lorem ipsum dolor sit amet consectetur, 
-                        adipisicing elit. Sit, maiores!
-                        Lorem ipsum dolor sit amet consectetur 
-                        adipisicing elit. 
-                        A commodi enim id facilis, repellat ex perferendis 
-                        autem adipisci. Consectetur, optio!
+                </div>
+                <div class="sub_title_hair-space">
+                    <h3>smile</h3>
+                </div>
+                <div class="text_hair-space">
+                    <p>Lorem ipsum dolor sit amet, 
+                        consectetur adipisicing elit. 
+                        Nostrum, incidunt.
                         </p>
-                    </div>
-                </div>
-                <div class="card_passions">
-                    <div class="image_passions">
-                        <div class="image_passions">
-                            <img src="./inc/images/smile.svg" alt="book">
-                        </div>
-                    </div>
-                    <div class="sub_title_passions">
-                        <h3>smile</h3>
-                    </div>
-                    <div class="text_passions">
-                        <p>Lorem ipsum dolor sit amet consectetur, 
-                            adipisicing elit. Sit, maiores!
-                            Lorem ipsum dolor sit amet consectetur 
-                            adipisicing elit. 
-                            A commodi enim id facilis, repellat ex perferendis 
-                            autem adipisci. Consectetur, optio!
-                            </p>
-                    </div>
                 </div>
             </div>
         </div>
