65d85415db216a38c740c5ff9b8721ba986302e2
Andry <andry.rama.rahary@gmail.com>
2021-07-22 13:39:00 +0200

    ok

--- inc/haut.inc.php
+++ inc/haut.inc.php
@@ -10,7 +10,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,600;1,700&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,600;1,700&display=swap" rel="stylesheet">
+    <script src="<?php echo RACINE_SITE; ?>/inc/js/script.js"></script>
     <title>Document</title>
 </head>
 <body>
